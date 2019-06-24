99c217f9226afb4dc9feacf099c5c0218100a84b
Cloud <haohao10987@163.com>
2019-06-24 16:35:24 +0800

    no message

--- JNToolsDemo/JNToolsDemo/TestViewController.m
+++ JNToolsDemo/JNToolsDemo/TestViewController.m
@@ -26,13 +26,7 @@
 }
 
 - (void)jn_testMethod {
-    NSString *urlString = @"https://www.zhiliangwang.com/zlmall/AppZlGoodInfoController/queryZlGoodInfoPageListByCondittion.json?condition&pageSize=15&priceend&pricestart&processor.pageIndex=1&zlGoodsInfo.goodname&zlGoodsInfo.goodtypet=edd10b7fe7404ae2a6c13f5a6831e3a6&zlGoodsInfo.groupon=0&zlGoodsInfo.regionalid=b31244d6cc1e4bc1b843a70348565a7f";
-    [JNNetworkManager jn_getRequestWithUrlString:urlString parameters:nil success:^(id  _Nonnull responObject) {
-        JNLog(@"%@", responObject);
-        
-    } failure:^(NSError * _Nonnull error) {
-//        JNLog(@"%@", error);
-    }];
+    
 }
 
 @end
